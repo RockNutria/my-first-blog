a704dcd5b8b572d04b55e6c8331516b0b85f646f
RockNutria <zacharymbergman@gmail.com>
2024-04-28 11:42:27 -0400

    changed this fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+6v8z^rl@*1^+w*2dtq-k9%z!fy4m9^dh*mihydl%(ex4n)ex!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
